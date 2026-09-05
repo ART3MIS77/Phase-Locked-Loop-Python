58f24fec38381581f98428b59538852632ab3907
ART3MIS77 <73394407+ART3MIS77@users.noreply.github.com>
2026-09-05 23:25:38 +0300

    PLL v 2.1
    Cleaned the code. Should be more legible and work better

--- PLL v1.0.1.py
+++ PLL v1.0.1.py
@@ -3,85 +3,95 @@ from scipy import signal
 import matplotlib.pyplot as plt
 
 
-phase_vco = 0
-fs = 200
+fs = 250
 dt = 1.0 / fs
 t = np.arange(0, 1.0, 1.0 / fs)
 noise = np.random.normal(0,0.2,len(t))
 
-ref =5
-vco = 5
-f0 = 5
-phase_vco += 2 * np.pi * vco * dt
-sine_ref = np.sin(2 * np.pi * ref * t + np.pi /4)
+phase_vco = 0
+ref = 5
+f0 = 5                               # vco free frequency
+sine_ref = np.sin(2 * np.pi * ref * t + 2 * np.pi / 3)
 
-Kp = 0.5
-Ki = 0.05
+Kp = 1.1
+Ki = 0.9
 k0 = 1
 integral = 0
 
-def pi_controller(kp, ki, integral, error, time):
+
+def pi_controller(kp, ki, integral, error, dt):
   integral += error * dt
   P = kp * error
   I = ki * integral
   pi = P+I
 
-  return pi,integral
+  return pi,integral, P, I
 
 sine_ref_n = sine_ref + noise
 square_ref = sine_ref_n >= 0                               # sine -> square wave
-
+                                                           # square_ref = np.zeros(len(t), dtype=bool)
 
 phase_detector = []
 ref_out = []
 pi_result = []
+p_result = []
+i_result = []
 sine_vco = np.zeros(len(t))
-f_vco = np.zeros(len(t))
+square_vco = np.zeros(len(t))
 
-square_vco = sine_vco >= 0
-
-ref_out = np.zeros(len(t))
-
-phase_ref = 2 * np.pi * ref * t + np.pi / 4
-phase_vco_arr = 2 * np.pi * vco * t
+current_vco = f0
 
 for i in range(len(t)):
+  phase_vco += 2 * np.pi * current_vco * dt
+
+  sine_vco[i] = np.sin(phase_vco)
+  square_vco[i] = sine_vco[i] >= 0
 
   phase_detect = square_vco[i] != square_ref[i]
   phase_detector.append(phase_detect)
 
-  phase_error = phase_ref[i] - phase_vco
+  #phase_error = phase_ref[i] - phase_vco  | funny thing is that it works the same as XOR phase detection. if you put phase error or phase detect it will give the exact same result
 
-  pi_val, integral = pi_controller(Kp, Ki, integral, phase_error, dt)
+  pi_val, integral, p_val, i_val = pi_controller(Kp, Ki, integral, phase_detect, dt)
   pi_result.append(pi_val)
-  ref_out[i] = sine_ref_n[i] - pi_val
+  p_result.append(p_val)
+  i_result.append(i_val)
 
   current_vco = f0 + k0 * pi_val
+  ref_out.append(sine_vco[i])
 
-  phase_vco += 2 * np.pi * vco * dt
-  f_vco[i] = current_vco
+  #phase_vco += 2 * np.pi * current_vco * dt
 
-  phase_vco += 2 * np.pi * current_vco * dt
-
-  sine_vco[i] = np.sin(phase_vco)
-
-  ref_out[i] = sine_vco[i]
-
-
-plt.figure()
-fig, axs = plt.subplots(3, 1, figsize=(10, 8), sharex=True)
+fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(10, 10), sharex=True)
 
 axs[0].plot(t, sine_ref_n, label='Reference (noisy)')
 axs[0].plot(t, sine_vco, label='VCO')
 axs[0].set_ylabel('Amplitude')
 axs[0].legend()
-axs[0].set_title('Reference and VCO signals')
 
-axs[1].plot(t, phase_detector, label='Phase Detector Output', color='orange')
-axs[1].set_ylabel('Phase Error (bool)')
+axs[1].plot(t, pi_result, label='PI controller', color='orange')
 axs[1].legend()
 
-axs[2].plot(t, ref_out, label='Reference (clean)', linestyle='--', alpha=0.6)
+axs[2].plot(t, ref_out, label='Recovered signal', linestyle='--', alpha=0.6, color='green')
 axs[2].set_ylabel('Amplitude')
 axs[2].legend()
+plt.show()
+
+
+fig, axs = plt.subplots(2, 1, figsize=(10, 8), sharex=True)
+axs[0].plot(t, square_ref, label='Square red')
+axs[0].plot(t, square_vco, label='Square vco')
+axs[0].legend()
+axs[1].plot(t, phase_detector, label='Phase Detector Output', color='orange')
+axs[1].legend()
+plt.show()
+
+fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(10, 10), sharex=True)
+axs[0].plot(t, p_result, label='Proportional')
+axs[0].legend()
+axs[1].plot(t, i_result, label='Integral')
+axs[1].legend()
+axs[2].plot(t, pi_result, label='P+I')
+axs[2].legend()
+
 plt.show()
